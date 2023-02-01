843b4f94ee0812d28cd9c3b9e6ab09bfb89af00f
Michael Murray <mmurray24@hotmail.com>
2023-02-01 15:57:15 +0000

    MVP completed

--- src/coffee_shop.py
+++ src/coffee_shop.py
@@ -1,5 +1,5 @@
 class CoffeeShop:
-    def __init__(self, name, till, drink =[]):
+    def __init__(self, name, till, drink):
         self.name = name
         self.till = till
         self.drink = drink
@@ -9,9 +9,21 @@ class CoffeeShop:
 
     def find_drink(self, drink_name):
         for drink in self.drink:
-            if drink["name"] == drink_name:
+            if drink.name == drink_name:
                 return drink
 
     def remove_drink(self, drink):
         # drink_found = self.find_drink(drink)
         self.drink.remove(drink)
+
+    
+    def sell_to_customer(self, drink, customer):
+        found_drink = self.find_drink(drink.name)
+        if found_drink == drink:
+            if customer.wallet >= drink.price:
+                self.increase_till(drink.price)
+                self.remove_drink(drink)
+                customer.remove_cash(drink.price)
+                customer.add_drink(drink)
+                    
+                
--- src/customer.py
+++ src/customer.py
@@ -3,3 +3,9 @@ class Customer:
         self.name = name
         self.wallet = wallet
         self.drink = []
+
+    def remove_cash(self,amount):
+        self.wallet -= amount
+
+    def add_drink(self, drink):
+        self.drink.append(drink)
