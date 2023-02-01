33062e928239fc56c2958c7c6b98e8e035954760
Michael Murray <mmurray24@hotmail.com>
2023-02-01 15:00:02 +0000

    additional commit

--- src/coffee_shop.py
+++ src/coffee_shop.py
@@ -1,7 +1,17 @@
 class CoffeeShop:
-    def __init__(self, name, till):
+    def __init__(self, name, till, drink =[]):
         self.name = name
         self.till = till
+        self.drink = drink
 
     def increase_till(self, amount):
         self.till += amount
+
+    def find_drink(self, drink_name):
+        for drink in self.drink:
+            if drink["name"] == drink_name:
+                return drink
+
+    def remove_drink(self, drink):
+        # drink_found = self.find_drink(drink)
+        self.drink.remove(drink)
--- tests/coffee_shop_test.py
+++ tests/coffee_shop_test.py
@@ -1,9 +1,19 @@
 import unittest
 from src.coffee_shop import CoffeeShop
+from src.customer import Customer
 
 class TestCoffeeShop(unittest.TestCase):
     def setUp(self):
-        self.coffee_shop = CoffeeShop('The Prancing Pony', 100.00)
+        self.drink_1 = {'name': "Hot Chocolate", 'price': 20}
+        self.drink_2 =  {'name': "Espresso", 'price': 15}
+        self.drink_3 = {'name': "Latte", 'price': 10}
+
+
+        drinks = [self.drink_1, self.drink_2, self.drink_3]
+        
+        self.coffee_shop = CoffeeShop('The Prancing Pony', 100.00, drinks)
+        self.customer = Customer("John Smith", 50)
+
 
 # testing class attributes
 
@@ -23,3 +33,20 @@ class TestCoffeeShop(unittest.TestCase):
     def test_increase_till(self):
         self.coffee_shop.increase_till(10.00)
         self.assertEqual(110.00,self.coffee_shop.till)
+
+    def test_has_drinks(self):
+        self.assertEqual(3,len(self.coffee_shop.drink))
+
+    def test_has_customer_name(self):
+        self.assertEqual("John Smith", self.customer.name)
+        
+    def test_has_customer_wallet(self):
+        self.assertEqual(50, self.customer.wallet)
+
+    def test_find_drink(self):
+        drink = self.coffee_shop.find_drink("Espresso")
+        self.assertEqual(self.drink_2, drink)
+
+    def test_remove_drink(self):
+        self.coffee_shop.drink.remove_drink(drink_1)
+        self.assertEqual(2,len(self.coffee_shop.drink))
